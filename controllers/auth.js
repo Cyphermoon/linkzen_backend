39a7bcae5e42b704cb6faa4213b11983cfc29297
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-10-12 23:25:14 -0700

    added send email verification functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,12 @@
 const User = require("../models/User");
 const CustomError = require("../errors/custom_error");
 const crypto = require("node:crypto");
+const sendVerificationEmail = require("../utils/sendVerificationEmail");
 
-const register = (req, res) => {
+const register = async (req, res) => {
   const { username, email, password } = req.body;
 
-  const emailExists = User.findOne({ email });
+  const emailExists = await User.findOne({ email });
   if (emailExists) {
     throw new CustomError.BadRequestError(
       "sorry, user with this email already exist"
@@ -13,16 +14,27 @@ const register = (req, res) => {
   }
 
   const verificationToken = crypto.randomBytes(40).toString("hex");
-
   const user = await User.create({
-    username, email, password, verificationToken
-  })
+    username,
+    email,
+    password,
+    verificationToken,
+  });
 
-  // frontend origin
-  const ORIGIN = process.env.ORIGIN
+  // frontend url
+  const origin = process.env.ORIGIN;
 
-  // TODO: write logic to send verification mail
-  
+  await sendVerificationEmail({
+    username: user.username,
+    email: user.email,
+    token: user.verificationToken,
+    origin: origin,
+  });
+
+  res.status(StatusCodes.CREATED).json({
+    msg: "Success! Please check your email to verify account",
+    token: verificationToken, //only for postman testing!
+  });
 };
 
 module.exports = register;
