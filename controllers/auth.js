d2f67ed8b5495bd61f5202efbe0bd18c4ae8aac2
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-10-13 15:26:03 -0700

    added verify email functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,11 @@
 const User = require("../models/User");
-const CustomError = require("../errors/custom_error");
+const UnAunthenticatedError = require("../errors/unauthenticated");
 const crypto = require("node:crypto");
 const sendVerificationEmail = require("../utils/sendVerificationEmail");
+const { StatusCodes } = require("http-status-codes");
 
 const register = async (req, res) => {
   const { username, email, password } = req.body;
-
-  const emailExists = await User.findOne({ email });
-  if (emailExists) {
-    throw new CustomError.BadRequestError(
-      "sorry, user with this email already exist"
-    );
-  }
 
   const verificationToken = crypto.randomBytes(40).toString("hex");
   const user = await User.create({
@@ -27,14 +21,35 @@ const register = async (req, res) => {
   await sendVerificationEmail({
     username: user.username,
     email: user.email,
-    token: user.verificationToken,
+    verificationToken: user.verificationToken,
     origin: origin,
   });
 
   res.status(StatusCodes.CREATED).json({
+    success: true,
     msg: "Success! Please check your email to verify account",
     token: verificationToken, //only for postman testing!
   });
 };
 
-module.exports = register;
+const verifyEmail = async (req, res) => {
+  const { verificationToken, email } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnAunthenticatedError("invalid credentials");
+  }
+
+  if (verificationToken != user.verificationToken) {
+    throw new UnAunthenticatedError("invalid credentials");
+  }
+  (user.isVerified = true), (user.active = true);
+  user.verified = Date.now();
+  user.verificationToken = "";
+
+  // save the user
+  await user.save();
+  res.status(StatusCodes.OK).json({ msg: "Email Verified" });
+};
+
+module.exports = { register, verifyEmail };
