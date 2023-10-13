0deb57b0ef4f7a914c665f9c760d1e0823b2f060
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-10-13 15:24:28 -0700

    updated error handler function

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,14 +1,37 @@
 const { StatusCodes } = require("http-status-codes");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  let errors = {};
+  console.log(err);
+  let errors = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong try again later",
+  };
+
   if (err.name === "ValidationError") {
-    errors.msg = Object.values(err.errors)
-      .map((item) => item.message)
-      .join(",");
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+    errors.statusCode = 400;
+    errors.msg = err.name;
+  }
+
+  // duplicate email error
+  if (err.code && err.code === 11000) {
+    errors.msg = `sorry, user with this ${Object.keys(
+      err.keyValue
+    )} already exists, please choose another ${Object.keys(err.keyValue)}`;
     errors.statusCode = 400;
   }
-  return res.status(errors.statusCode).json({ msg: errors.msg });
+
+  if (err.name === "CastError") {
+    errors.message = err.message;
+  }
+  // remove status code from the errors object
+  const { statusCode, ...details } = errors;
+
+  return res
+    .status(errors.statusCode)
+    .json({ success: false, errors: details });
 };
 
 module.exports = errorHandlerMiddleware;
