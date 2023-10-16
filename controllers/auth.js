b39cda9738a2af9d4b3c5b4fea75c4c1ea719ebc
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-10-16 15:37:01 -0700

    added login and logout functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,9 @@ const User = require("../models/User");
 const UnAunthenticatedError = require("../errors/unauthenticated");
 const crypto = require("node:crypto");
 const sendVerificationEmail = require("../utils/sendVerificationEmail");
+const { attachTokenToResponse, removeTokenFromResponse} = require('../utils/handleCookies')
 const { StatusCodes } = require("http-status-codes");
+
 
 const register = async (req, res) => {
   const { username, email, password } = req.body;
@@ -52,4 +54,33 @@ const verifyEmail = async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: "Email Verified" });
 };
 
-module.exports = { register, verifyEmail };
+const login = (req, res) => {
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
+  if (!user) {
+    throw new UnAunthenticatedError('invalid credentials')
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password)
+    if (!isPasswordCorrect) {
+    throw new UnAunthenticatedError('password is not correct')
+  }
+
+  if (!user.isVerified) {
+    throw new UnAunthenticatedError('please verify your email')
+  }
+
+  // create jwt for user
+  const token = user.createJWT()
+  // add token to response as a cookie
+  attachTokenToResponse(res, token)
+  res.status(StatusCodes.OK).json({success:true, user:{id:user._id, username:user.name}})
+}
+
+const logout = (req, res) => {
+  // update after creating isAuthenticated middleware
+  removeTokenFromResponse(res)
+}
+
+module.exports = { register, verifyEmail, login};
