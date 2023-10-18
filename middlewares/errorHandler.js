782ced68bd655c2e308d695ed3f9a4ea9822c463
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-10-18 14:54:28 -0700

    updated

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 const { StatusCodes } = require("http-status-codes");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err);
+  // console.log(err);
   let errors = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || "Something went wrong try again later",
