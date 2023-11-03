b0485c74a3190026a2a82fe028a6bd8d81722931
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-11-02 18:23:47 -0700

    updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require("express-async-errors");
 
 // third party
 const express = require("express");
-const cookieParser = require("cookie-parser");
+// const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
@@ -37,12 +37,12 @@ app.use(cors());
 app.use(helmet());
 app.use(mongoSanitize());
 app.use(express.json());
-app.use(cookieParser(process.env.JWT_SECRET));
+// app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.urlencoded({ extended: false }));
 
 // routes
 app.use("/api/v1/auth", authRouter);
-app.use("api/v1/profile", isUserAuthenticated, profileRouter);
+app.use("/api/v1/profile", isUserAuthenticated, profileRouter);
 
 app.use(errorHandlerMiddleware);
 
