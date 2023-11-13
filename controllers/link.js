00f3e7097f9e44b609f70fc6bd4cac820fde1d39
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-11-13 13:26:02 -0800

    added many to many operation

--- controllers/link.js
+++ controllers/link.js
@@ -1,10 +1,12 @@
 const BadRequestError = require("../errors/bad_request");
 const Link = require("../models/Link");
+const Tag = require("../models/Tags");
+const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 
 // dashboard links
 const getLinks = async (req, res) => {
-  const { sort } = req.query;
+  const { sort, layoutOption } = req.query;
   let sortCriteria = {};
 
   if (sort) {
@@ -41,7 +43,17 @@ const getLinks = async (req, res) => {
   } else {
     sortCriteria = { createdAt: -1 };
   }
-  const links = await Link.find({ createdBy: req.user.id }).sort(sortCriteria);
+
+  // update the user's layout preference
+  if (layoutOption) {
+    const user = await User.findById({ _id: req.user.id });
+    user.profile.layoutPreference = layoutOption;
+  }
+
+  // get the links
+  const links = await Link.find({ createdBy: req.user.id })
+    .sort(sortCriteria)
+    .populate("tags");
   if (links.length === 0) {
     return res.status(StatusCodes.OK).json({
       success: true,
@@ -57,12 +69,32 @@ const getLinks = async (req, res) => {
 
 const createNewLink = async (req, res) => {
   const { username } = req.params;
+
   if (username != req.user.username) {
     throw new BadRequestError("unable to create new link");
   }
+  const tagNames = req.body.tags;
+
+  let tagIds = [];
+
+  for (const tagName of tagNames) {
+    // check if tags exist
+    let tag = await Tag.find({ name: tagName });
+
+    // create new tags, if tags doesn't exist
+    if (!tag) {
+      tag = await Tag.create({ name: tagName });
+    }
+
+    // Add the tag to the array
+    tagIds.push(tag._id);
+  }
+
+  req.body.tags = tagIds;
 
   req.body.createdBy = req.user.id;
   const link = await Link.create(req.body);
+
   res
     .status(StatusCodes.CREATED)
     .json({ success: true, msg: "link created successfully", data: link });
@@ -72,28 +104,41 @@ const updateLink = async (req, res) => {
   // id of the link
   const { id } = req.params;
 
-  req.body.createdBy = req.user.id;
-  const link = await Link.findOneAndUpdate(
-    { _id: id, createdBy: req.user.id },
-    req.body,
-    { new: true, runValidators: true }
-  );
-  if (!link) {
+  let existingLink = await Link.findOne({ _id: id, createdBy: req.user.id });
+  if (!existingLink) {
     return res.status(StatusCodes.NOT_FOUND).json({
       success: false,
       message: "unable to update link",
     });
   }
+
+  // assign user to body
+  req.body.createdBy = req.user.id;
+
+  const newLink = await Link.findOneAndUpdate(
+    { _id: id, createdBy: req.user.id },
+    {
+      $set: {
+        title: req.body.title || existingLink.title,
+        url: req.body.url || existingLink.url,
+        description: req.body.description || existingLink.description,
+      },
+      $addToSet: { tags: { $each: req.body.tags || existingLink.tags || [] } },
+    },
+
+    { new: true, runValidators: true }
+  );
+
   res
     .status(StatusCodes.OK)
-    .json({ success: true, msg: "link updated successfully", data: link });
+    .json({ success: true, msg: "link updated successfully", data: newLink });
 };
 
 const deleteLink = async (req, res) => {
   const { id } = req.params;
   req.body.createdBy = req.user.id;
 
-  const link = await Link.findByIdAndRemove({
+  const link = await Link.findOneAndDelete({
     _id: id,
     createdBy: req.user.id,
   });
